9415e82cea57f282958bcd674ca4f74dcd2a3b17
ussi-kousuke <houshanangsuke3@gmail.com>
2022-12-07 03:05:25 +0900

    Default Changelist

---
+++ oop-coffee-machine-start/main.py
@@ -0,0 +1,9 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
